d9ce589c3342597c9e1f9b9add651645c7872b08
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-12-04 12:48:00 +0100

    script: added reset and interactable

--- Assets/MyGame/Scripts/ComputeNumbers.cs
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -6,12 +6,30 @@ public class ComputeNumbers : MonoBehaviour
     public InputField ip_varA;
     public InputField ip_varB;
     public Text result;
+    public Button Btn_Add;
+    public Button Btn_Reset;
+
 
     public void SetResult()
     {
         float a = float.Parse(ip_varA.text);
         float b = float.Parse(ip_varB.text);
         result.text = AddNumbers(a, b).ToString();
+        ip_varA.interactable = false;
+        ip_varB.interactable = false;
+        Btn_Add.interactable = false;
+        Btn_Reset.interactable = true;
+    }
+
+    public void Reset()
+    {
+        ip_varA.text = "0";
+        ip_varB.text = "0";
+        result.text = "0";
+        ip_varA.interactable = true;
+        ip_varB.interactable = true;
+        Btn_Add.interactable = true;
+        Btn_Reset.interactable = false;
     }
 
     private float AddNumbers(float variableA, float variableB)
