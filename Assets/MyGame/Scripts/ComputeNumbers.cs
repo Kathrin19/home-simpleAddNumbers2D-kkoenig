6f6d8b425fafef398e375ab2e6450efc1900d9d2
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-12-04 12:35:38 +0100

    script version: adding numbers

---
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeNumbers : MonoBehaviour
+{
+    public InputField ip_varA;
+    public InputField ip_varB;
+    public Text result;
+
+    public void SetResult()
+    {
+        float a = float.Parse(ip_varA.text);
+        float b = float.Parse(ip_varB.text);
+        result.text = AddNumbers(a, b).ToString();
+    }
+
+    private float AddNumbers(float variableA, float variableB)
+    {
+        float result = variableA + variableB;
+        return result;
+    }
+}
